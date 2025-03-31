9c133c21b6edf74c39d00cf053e315afb6a986fd
Newen Fara <newen.fara@payaut.com>
2025-03-31 20:49:50 +0200

    Init React app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Swift Reader Practice</h1>
+      <div className="card">
+        <p>This is a website to motivate children to practice reading by keeping track of the time.</p>
+        <p>The idea is that making a better time becomes a motivation for them to practice more.</p>
+      </div>
+    </>
+  )
+}
+
+export default App

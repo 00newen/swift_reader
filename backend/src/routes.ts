9a1e96a66b0f8f3cc15ea980b046df15bab1a1e1
Newen Fara <newen.fara@payaut.com>
2025-04-06 10:32:01 +0200

    implement GET sessions/:session-code endpoint

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -37,8 +37,32 @@ v1Router.get('/session', async (req: Request, res: Response) => {
   }
 });
 
-v1Router.get('/sessions/:session_code', (req: Request, res: Response) => {
-  res.json({ message: `Fetching session ${req.params.session_code}` });
+v1Router.get('/sessions/:session_code', async (req: Request, res: Response) => {
+  const sessionCode = req.params.session_code;
+  let sessionExists = false;
+  let sessionId = null;
+
+  try {
+    const result = await pgPool.query('SELECT id FROM "swift-reader_sessions" WHERE code = $1', [sessionCode]);
+    if (result.rows.length > 0) {
+      sessionExists = true;
+      sessionId = result.rows[0].id;
+    } else {
+      const insertResult = await pgPool.query('INSERT INTO "swift-reader_sessions" (code) VALUES ($1) RETURNING id', [
+        sessionCode,
+      ]);
+      sessionId = insertResult.rows[0].id;
+    }
+
+    res.status(200).json({
+      message: `Session ${sessionCode} ${result.rows.length > 0 ? 'fetched' : 'created'} `,
+      code: sessionCode,
+      id: sessionId,
+    });
+  } catch (error) {
+    console.error('Error fetching session:', error);
+    res.status(500).json({ message: 'Failed to fetch session' });
+  }
 });
 
 // v1Router.post('/sessions', (req: Request, res: Response) => {
